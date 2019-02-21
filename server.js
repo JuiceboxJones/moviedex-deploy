0de84cf751b7d8db1274c3491fadef78e55dbefb
Juicebox Jones <jderhalli@gmail.com>
2019-02-21 11:40:02 -0800

    completed moviedex

--- server.js
+++ server.js
@@ -6,7 +6,46 @@ const movies = require('./movies.json');
 const helmet = require('helmet');
 const cors = require('cors');
 const app = express();
+require('dotenv').config();
 
 app.use(helmet());
 app.use(morgan());
 app.use(cors());
+
+app.use((req, res, next) => {
+  const authToken = req.get('Authorization');
+  const API_TOKEN = process.env.API_TOKEN;
+  if(!authToken || authToken.split(' ')[1] !== API_TOKEN){
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  next();
+});
+
+app.get('/movies', (req, res) => {
+  let response = movies;
+  const { genre, country, avg_vote } = req.query;
+
+  if (genre) {
+    response = response.filter(movie =>
+      movie.genre.toLowerCase().includes(genre.toLowerCase())
+    );
+  }
+
+  if (country) {
+    response = response.filter(movie =>
+      movie.country.toLowerCase().includes(country.toLowerCase())
+    );
+  }
+
+  if (avg_vote) {
+    response = response.filter(movie =>
+      movie.avg_vote >= avg_vote);
+  }
+
+  res.send(response);
+});
+
+app.listen(8000, () => {
+  console.log('Server is listening on port 8000');
+});
